153364bdaa686a4055a75697d989a1e80470d53b
Jyde24 <chukwunonsojide.e@gmail.com>
2023-09-03 17:06:12 +0000

    RSA Factoring Challenge

--- factors.c
+++ factors.c
@@ -1,46 +1,63 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
+/**
+ * factorize - Factorize a number into two smaller integers.
+ * @n: The number to factorize.
+ *
+ * This function attempts to find a factorization of the input number @n into
+ * two smaller integers, @i and @j. It prints the factorization in the format
+ * "n=i*j". If no factorization is found, it assumes that the input number is
+ * prime and prints it in the format "n=n*1".
+ */
+
+void factorize(int n)
 {
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	long long flag = 1, div, rest, number, counter;
-	ssize_t nread;
-
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((nread = getline(&line, &len, stream)) != -1) {
-		flag = 1, div = 2;
-		number = atoll(line);
-		while (flag) {
-			rest = number % div;
-			if (!rest) {
-				counter = number / div;
-				printf("%lld=%lld*%lld\n", number, counter, div);
-				flag = 0;
-			}
-			div++;
+	int x, y;
+	for (x = 2; y <= n / 2; ++x)
+	{
+		if (n % x == 0)
+		{
+			y = n / x;
+			printf("%d=%d*%d\n", n, x, y);
+			return;
 		}
 	}
+	printf("%d is a prime number\n", n);
+}
 
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * This function reads numbers from a file specified in the command-line
+ * arguments and calls the factorize function to factorize each number.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <file>\n", argv[0]);
+		return 1;
+	}
+
+	FILE *file;
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		printf("Error: Could not open file %s\n", argv[1]);
+		return 1;
+	}
+
+	int num;
+	while (fscanf(file, "%d", &num) != EOF)
+	{
+		factorize(num);
+	}
+
+	fclose(file);
+
+	return 0;
 }
